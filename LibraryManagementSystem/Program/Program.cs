dc95ca277701afe3a49be81b9fe5bd5addaaf648
SaberOrien <shino097531@gmail.com>
2024-04-15 21:39:55 +0200

    First task added data layer

---
+++ LibraryManagementSystem/Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using LibraryData;
+using LibraryLogic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create a new service collection
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        // Build the provider
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Run your application code
+        var manager = serviceProvider.GetService<LibraryManager>();
+        manager.AddItem(new Item { Id = 1, Title = "Sample Book", Author = "Author", PublishingYear = 2021, Publisher = "Publisher" });
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // Add the LibraryContext as a singleton since we want to maintain a single instance throughout the application
+        services.AddSingleton<LibraryContext>();
+
+        // Add LibraryManager as a transient or scoped service depending on your needs
+        services.AddTransient<LibraryManager>();
+    }
+}
---
+++ LibraryManagementSystem/Reader.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagementSystem.Data
+
+{
+    public class Reader
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfSignUp { get; set; }
+        public List<BorrowRecord> BooksBorrowed { get; set; } = new List<BorrowRecord>();
+    }
+}
